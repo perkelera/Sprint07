68ddef7560af5dd031eb50972b8e00f8521918a7
Anton <anton-rachkov@mail.ru>
2026-06-29 18:44:09 +0200

    Initial commit

---
+++ ImageFeed/ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by никита  on 29.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
