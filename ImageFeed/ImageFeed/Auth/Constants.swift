3b6aa85c6df4f42062a01b8b87b745ed0b7d1ee5
perkelera <anton-rachkov@mail.ru>
2026-08-01 15:34:28 +0300

    no message

---
+++ ImageFeed/ImageFeed/Auth/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by никита  on 21.07.2026.
+//
+
+import Foundation
+enum Constants {
+    static let accessKey = "XFc3H_S2d7rtw9pRkZJgDMvN4pfSri59BbvnofKBvp8"
+    static let secretKey = "JOvdebsEzp-TFWL4YjiyuQnItPQZ-IY6fdjNWnAdfQw"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURLString = "https://api.unsplash.com"
+}
---
+++ ImageFeed/ImageFeed/Auth/OAuth2Service.swift
@@ -0,0 +1,62 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//  Created by Anton Rachkov on 31.07.2026.
+//
+import Foundation
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    
+    private init() {}
+    
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token") else {
+            return nil
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let authTokenUrl = urlComponents.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: authTokenUrl)
+        request.httpMethod = "POST"
+        return request
+    }
+    
+    func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let request = makeOAuthTokenRequest(code: code) else {
+            completion(.failure(NetworkError.invalidRequest))
+            return
+        }
+        
+        let task = URLSession.shared.data(for: request) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let response = try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                    OAuth2TokenStorage.shared.token = response.accessToken
+                    completion(.success(response.accessToken))
+                } catch {
+                    print("❌ Decoding error: \(error)")
+                    completion(.failure(NetworkError.decodingError(error)))
+                }
+            case .failure(let error):
+                print("❌ Network error: \(error)")
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
